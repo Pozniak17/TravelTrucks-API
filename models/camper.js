1dc431e101a110700cef0d7c0913a8ae829a299e
Yevhenii Pozniak <jekamanu@gmail.com>
2025-11-20 14:35:00 +0200

    update

--- models/camper.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-const camperSchema = new mongoose.Schema();
-
-export default mongoose.model("Camper", camperSchema);
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import camperRoutes from "./campers.js";
-
-const router = express.Router();
-
-router.use("/campers", camperRoutes);
-
-export default router;
